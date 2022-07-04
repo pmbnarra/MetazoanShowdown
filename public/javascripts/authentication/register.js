bc231635991ed1995bd0482755ff0970238ef722
pmbnarra <108690403+pmbnarra@users.noreply.github.com>
2022-07-04 18:55:12 +0100

    Update register.js

--- public/javascripts/authentication/register.js
+++ public/javascripts/authentication/register.js
@@ -2,9 +2,16 @@ async function registerPlayer() {
     try {
         let name = document.getElementById("name").value;
         let password = document.getElementById("password").value;
-        let res = await register(name,password);
-        alert(res.msg);    
-        window.location = "index.html";  
+        
+        var regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+        if (!regex.test(password)){
+            alert("The password is not secure. It must be longer than 8 characters, include a number, an uppercase letter and a lowercase letter");   
+        }else{
+            let res = await register(name,password);
+            alert(res.msg);    
+            window.location = "index.html";  
+        }
+
     } catch (err) {
         console.log(err);
     }
